e4ce21acb371762d3264fa757b59747ddbc298c0
mehrshad-kh <mjii@protonmail.com>
2023-10-03 20:13:49 +0330

    feat (movies.continue): improve call to curl

--- movies.continue.sh
+++ movies.continue.sh
@@ -7,4 +7,4 @@ last_episode_filename=$(readlink latest)
 first_episode_link="$(cat .info | grep "^url" | cut -d "=" -f 2 | xargs)"
 new_episode_link="$(echo ${first_episode_link} | rev | cut -d "/" -f 2- | rev)/${last_episode_filename}"
 
-curl -L -g -O -C - --retry-all-errors ${new_episode_link}
+curl -L -O -C - -f --retry-all-errors --retry-max-time 120 ${new_episode_link}
